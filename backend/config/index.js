eec5b125f08263d90b42ff1a7a19b6c7f084874a
牧元 <wmy259710@alibaba-inc.com>
2026-06-03 19:11:38 +0800

    feat: 添加DEF部署配置，支持环境变量PORT

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-  PORT: 3000,
+  PORT: process.env.PORT || 3000,
   DB_PATH: "./db/disney.db",
   WAITTIME_UPDATE_INTERVAL: "*/5 * * * *",
   CORS_ORIGIN: "http://localhost:5500",
